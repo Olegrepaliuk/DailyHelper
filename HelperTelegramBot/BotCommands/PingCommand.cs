fb1ba238d1a61e94801c5d37ff830736b0d13690
Oleg <Oleg58807@ukr.net>
2023-08-04 00:05:35 +0300

    Added ping command

---
+++ HelperTelegramBot/BotCommands/PingCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace HelperTelegramBot.BotCommands
+{
+    public class PingCommand : ICommand
+    {
+        public string CommandName => "/ping";
+
+        public async Task Execute(ITelegramBotClient botClient, Update update)
+        {
+            string answerText = $"How can I help you?";
+
+            await botClient.SendTextMessageAsync(update.Message.Chat, answerText);
+        }
+    }
+}
