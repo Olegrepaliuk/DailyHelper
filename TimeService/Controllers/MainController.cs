20a7f8a7b807d1ed37a6db01dde76978b3519fb1
Oleg <Oleg58807@ukr.net>
2023-08-15 00:57:00 +0300

    added main controller for TimeService

---
+++ TimeService/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeService.Controllers
+{
+    public class MainController : Controller
+    {
+        [Route("api/main")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
